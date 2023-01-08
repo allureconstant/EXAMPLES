6e728cb99176504dedb2027dbcd8f3f0523cd6f0
Constantin Filippov <filippovcos@mail.ru>
2023-01-08 23:16:57 +0300

    add exmaple 006 base

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите имя пользователя:");
 string username = Console.ReadLine();
 
-if(username.ToLower () == "Маша")
+if(username.ToLower() == "Маша")
 {
     Console.WriteLine("Ура, это же МАША");
 }
